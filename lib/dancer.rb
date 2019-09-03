66ebb867d19f699b34d09953ff7ba1e91810a24e
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-03 04:09:00 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,5 +5,9 @@ require_relative './fancy_dance.rb'
 class Dancer
   include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
-  
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
