93abe486cf253a95b809b2c8254e02c7d8b5c4c0
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-03 03:54:16 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,5 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  extend MetaDancing
-end
+  extend 
+  FancyDance::ClassMethods
+  include 
+  FancyDance::InstanceMethods
+end 
